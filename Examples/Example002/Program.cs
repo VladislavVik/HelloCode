57f746d21ac4ee27a48033b0d5a3a389d970f933
Vladisalv Volkov <vlad16396@gmail.com>
2023-06-08 23:00:06 +0300

    new

--- Examples/Example002/Program.cs
+++ Examples/Example002/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите Ваше имя");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+// Console.Write("Введите Ваше имя");
+// string username = Console.ReadLine();
+// Console.Write("Привет, ");
+// Console.Write (username);
